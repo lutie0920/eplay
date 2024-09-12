d88c7e9a38298a33f6bf17d9693b2e3fcebf1f97
lutie0920 <lucianatie09@gmail.com>
2024-09-12 16:55:02 -0300

    first commit

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,102 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import fifa from '../../assets/images/fifa.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4 - Remake',
+    system: 'Windows',
+    infos: ['R$199,99', '-10%'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4 - Remake',
+    system: 'Windows',
+    infos: ['R$199,99', '-10%'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['R$99,90', '-50%'],
+    image: fifa
+  },
+  {
+    id: 4,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['R$99,90', '-50%'],
+    image: fifa
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4 - Remake',
+    system: 'Windows',
+    infos: ['R$199,99', '-10%'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4 - Remake',
+    system: 'Windows',
+    infos: ['R$199,99', '-10%'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['R$99,90', '-50%'],
+    image: fifa
+  },
+  {
+    id: 4,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['R$99,90', '-50%'],
+    image: fifa
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,106 @@
+import Banner from '../../components/Banner'
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import fifa from '../../assets/images/fifa.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+import streetFighter from '../../assets/images/street_fighter.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4 - Remake',
+    system: 'Windows',
+    infos: ['R$199,99', '-10%'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4 - Remake',
+    system: 'Windows',
+    infos: ['R$199,99', '-10%'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['R$99,90', '-50%'],
+    image: fifa
+  },
+  {
+    id: 4,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['R$99,90', '-50%'],
+    image: fifa
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo 4',
+    system: 'Windows',
+    infos: ['05/04'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star Wars Jedi Survivor',
+    system: 'Windows',
+    infos: ['05/04'],
+    image: starWars
+  },
+  {
+    id: 7,
+    category: 'Luta',
+    description:
+      'Street Fighter 6 é um próximo jogo de luta desenvolvido e publicado pela Capcom.',
+    title: 'Street Fighter 6',
+    system: 'Windows',
+    infos: ['05/04'],
+    image: streetFighter
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    title: 'The Legend of Zelda - TOK',
+    system: 'Switch',
+    infos: ['05/04'],
+    image: zelda
+  }
+]
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductsList games={promocoes} title="Promoções" background="gray" />
+    <ProductsList games={emBreve} title="Em breve" background="black" />
+  </>
+)
+
+export default Home
