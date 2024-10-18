b40bcc4343acb88dabc61c10e321616c51fa2b4b
lutie0920 <lucianatie09@gmail.com>
2024-10-18 15:59:31 -0300

    feat: integrar com a API ll

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,3 +1,4 @@
+import { idText } from 'typescript'
 import Tag from '../Tag'
 
 import { Card, Descricao, Titulo, Infos } from './styles'
@@ -9,6 +10,7 @@ type Props = {
   description: string
   infos: string[]
   image: string
+  id: number
 }
 
 const Product = ({
@@ -17,7 +19,8 @@ const Product = ({
   system,
   description,
   infos,
-  image
+  image,
+  id
 }: Props) => {
   const getDescricao = (descricao: string) => {
     if (descricao.length > 95) {
@@ -27,7 +30,7 @@ const Product = ({
   }
 
   return (
-    <Card>
+    <Card to={`/product/${id}`}>
       <img src={image} alt={title} />
       <Infos>
         {infos.map((info) => (
